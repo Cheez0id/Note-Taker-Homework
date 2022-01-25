7468d5297a58d9cbd27003598add8f9e7cc9eda6
cheez0idgit <93955021+Chee0id@users.noreply.github.com>
2022-01-24 20:20:54 -0500

    added .html to link in index.html to bring up the notes page onclick

--- server.js
+++ server.js
@@ -5,9 +5,19 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.send('Visit http://localhost:3001/index.html'));
+// app.get('/', (req, res) => res.send('Visit http://localhost:3001/index.html'));
+
+
+
+//TODO: the below should take us to the notes html, not a response with the string below
+app.get('/notes', (req, res) => res.send('Visit http://localhost:3001/notes.html'));
+
 
 app.listen(PORT, () =>
   console.log(`app listening at http://localhost:${PORT}`)
